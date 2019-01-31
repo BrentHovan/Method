f5cd4f6dadeae10ee92135441ea3fbb6c73def23
Brent Hovan <brenthovan@gmail.com>
2019-01-31 16:15:47 -0500

    Method Deliverable: see ReadMe for requirements

--- Method/Program.cs
+++ Method/Program.cs
@@ -6,7 +6,23 @@ namespace Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+                Greet_User(name);
+            }
+            catch
+            {
+                Console.WriteLine("Your 'name' is apparently incomprehensible...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void Greet_User(string get_name)
+        {
+            Console.WriteLine("Hello " + get_name + "!");
+            Console.ReadKey();
         }
     }
 }
